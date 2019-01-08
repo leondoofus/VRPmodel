d5b0bf496b58965fb147a4a1e63e29dd1cca2be0
leondoofus <teenage_96@yahoo.com>
2019-01-08 18:21:27 +0100

    MTZ and clean

--- Project/Project/MTZ.cpp
+++ Project/Project/MTZ.cpp
@@ -11,6 +11,56 @@
 
 using namespace std;
 
+//#define OUTPUT
+
+// ILOLAZYCONSTRAINTCALLBACK2(LazyMengerCutSeparation,
+// 			   C_Graph &, G,			 
+// 			   vector<vector<IloNumVar> >&,x
+// 		    ){
+//   #ifdef OUTPUT
+//   cout<<"*********** Lazy separation Callback *************"<<endl;
+//   #endif
+
+//   int i;
+//   list<C_link *>::const_iterator it;
+//   IloRange ViolatedCst;
+  
+//   // Put the INTEGER values x on the edges of graph G
+//   // Be carefull... Cplex can send -0.00001 for 0 or 0.000099 for 1
+
+//   for (i=0;i<G.nb_nodes;i++)
+//     for (it=G.V_nodes[i].L_adjLinks.begin();it!=G.V_nodes[i].L_adjLinks.end();it++){
+//       if (i<(*it)->return_other_extrem(i)){
+// 		(*it)->algo_cost= getValue(x[i][(*it)->return_other_extrem(i)]);
+// 		if((*it)->algo_cost<epsilon) (*it)->algo_cost=0 ;
+// 		else (*it)->algo_cost=1 ;
+//       }
+//     }
+
+//   /* Separation of Cut inequalities */
+
+//   if (find_ViolatedMengerCutCst_INTEGER(getEnv(),G,x, ViolatedCst)){
+//     #ifdef OUTPUT
+//     cout << "Adding constraint : "<<endl;
+//     cout<< ViolatedCst << endl;
+//     #endif
+//     add(ViolatedCst,IloCplex::UseCutPurge);   // UseCutForce UseCutPurge UseCutFilter
+//   }
+//   #ifdef OUTPUT
+//     else {
+//       cout<<"No Cst found"<<endl;
+//     }
+//   #endif 
+
+// }
+
+
+
+
+
+
+
+
 void MTZ::compute(Graph *graph) {
 	string name, nameext, nameextsol;
 	int i, j, k;
@@ -62,15 +112,18 @@ void MTZ::compute(Graph *graph) {
 	//////  VAR
 	////////////////////////
 
-	vector<IloNumVar> x;
-	x.resize(graph->dimension*graph->dimension);
+	vector<vector<IloNumVar>> x;
+	x.resize(graph->dimension);
+	for (i=0;i<graph->dimension;i++)
+    	x[i].resize(graph->dimension);
+
 	for (i = 0; i < graph->dimension; i++) {
 		for (j = 0; j < graph->dimension; j++) {
-			x[i * graph->dimension + j] = IloNumVar(env, 0.0, 1.0, ILOINT);
+			x[i][j] = IloNumVar(env, 0.0, 1.0, ILOINT);
 			ostringstream varname;
 			varname.str("");
 			varname << "x_" << i << "_" << j;
-			x[i * graph->dimension + j].setName(varname.str().c_str());
+			x[i][j].setName(varname.str().c_str());
 		}
 	}
 
@@ -99,8 +152,8 @@ void MTZ::compute(Graph *graph) {
 	IloExpr c2(env);
 	for (i = 0; i < graph->dimension; i++) {
 		if (i != depot) {
-			c1 += x[depot * graph->dimension + i];
-			c2 += x[i * graph->dimension + depot];
+			c1 += x[depot][i];
+			c2 += x[i][depot];
 		}
 	}
 	CC.add(c1 <= graph->vehicles);
@@ -117,8 +170,8 @@ void MTZ::compute(Graph *graph) {
 			IloExpr c4(env);
 			for (j = 0; j < graph->dimension; j++) {
 				if (j != i) {
-					c3 += x[i * graph->dimension + j];
-					c4 += x[j * graph->dimension + i];
+					c3 += x[i][j];
+					c4 += x[j][i];
 				}
 			}
 			CC.add(c3 == 1);
@@ -146,7 +199,7 @@ void MTZ::compute(Graph *graph) {
 		for (j = 0; j < graph->dimension; j++) {
 			if (j != i) {
 				IloExpr c5(env);
-				c5 += w[i] - w[j] - (Q + graph->demand[i]) * x[i * graph->dimension + j];
+				c5 += w[i] - w[j] - (Q + graph->demand[i]) * x[i][j];
 				CC.add(c5 >= -Q);
 				ostringstream nomcst;
 				nomcst.str("");
@@ -156,6 +209,19 @@ void MTZ::compute(Graph *graph) {
 			}
 		}
 	}
+
+	// Trivial Constraints
+	for (i = 0; i < graph->dimension; i++) {
+		IloExpr c6(env);
+		c6 += x[i][i];
+		CC.add(x[i][i] == 0);
+		ostringstream nomcst;
+		nomcst.str("");
+		nomcst << "CstT_" << i;
+		CC[nbcst].setName(nomcst.str().c_str());
+		nbcst++;
+	}
+
 
 	model.add(CC);
 
@@ -167,7 +233,7 @@ void MTZ::compute(Graph *graph) {
 
 	for (i = 0; i < graph->dimension; i++)
 		for (j = 0; j < graph->dimension; j++)
-			obj.setLinearCoef(x[i * graph->dimension + j], graph->distance(i+1,j+1));
+			obj.setLinearCoef(x[i][j], graph->distance(i+1,j+1));
 
 	///////////
 	//// RESOLUTION
@@ -205,12 +271,15 @@ void MTZ::compute(Graph *graph) {
 	env.out() << "Solution value  = " << cplex.getObjValue() << endl;
 
 
-	vector<int>   solx;
-	solx.resize(graph->dimension*graph->dimension);
+	vector<vector<int>>   solx;
+	solx.resize(graph->dimension);
+	for (i=0;i<graph->dimension;i++)
+    	solx[i].resize(graph->dimension);
+
 	for (i = 0; i < graph->dimension; i++) {
 		for (j = 0; j < graph->dimension; j++) {
-			solx[i * graph->dimension + j] = cplex.getValue(x[i * graph->dimension + j]);
-			//cout << solx[i * graph->dimension + j] << endl;
+			solx[i][j] = cplex.getValue(x[i][j]);
+			//cout << solx[i][j] << endl;
 		}
 	}
 
